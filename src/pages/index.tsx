d9b551be13de2883c133f0573272dc1749c48190
tsunose <tsunose.developer@gmail.com>
2020-08-02 14:56:57 +0900

    move src directory

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export const Top = (): JSX.Element => (
+  <div className="container">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main>
+      <h1 className="title">
+        TOPページです
+      </h1>
+    </main>]
+  </div>
+)
+
+export default Top
